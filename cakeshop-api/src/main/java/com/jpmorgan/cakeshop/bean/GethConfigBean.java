02b2926bf645ed7a828d8cf25df088f10a8762cc
Michael Kazansky <michael.kazansky@jpmchase.com>
2017-02-17 14:54:41 -0500

    moved constellation settings into other directory so it won't be purged during reset

--- cakeshop-api/src/main/java/com/jpmorgan/cakeshop/bean/GethConfigBean.java
+++ cakeshop-api/src/main/java/com/jpmorgan/cakeshop/bean/GethConfigBean.java
@@ -30,42 +30,42 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GethConfigBean {
-
+    
     private static final Logger LOG = LoggerFactory.getLogger(GethConfigBean.class);
-
+    
     public static final String START_LINUX_COMMAND = "bin/linux/geth";
     public static final String START_WIN_COMMAND = "bin/win/geth.exe";
     public static final String START_MAC_COMMAND = "bin/mac/geth";
-
+    
     @Autowired
     private Environment env;
-
+    
     @Value("${config.path}")
     private String CONFIG_ROOT;
-
+    
     @Autowired
     private QuorumConfigBean quorumConfig;
-
+    
     private String configFile;
-
+    
     private String binPath;
-
+    
     private String gethPath;
-
+    
     private String gethPidFilename;
-
+    
     private String constPidFileName;
-
+    
     private String gethPasswordFile;
-
+    
     private String genesisBlockFilename;
-
+    
     private String keystorePath;
-
+    
     private String nodePath;
-
+    
     private String solcPath;
-
+    
     private String publicKey;
 
     /**
@@ -73,11 +73,11 @@ public class GethConfigBean {
      */
     private Boolean isQuorum;
     private boolean isEmbeddedQuorum;
-
+    
     private Properties props;
-
+    
     private static final String DEFAULT_NODE_PORT = "30303";
-
+    
     private final String GETH_DATA_DIR = "geth.datadir";
     private final String GETH_LOG_DIR = "geth.log";
     private final String GETH_RPC_URL = "geth.url";
@@ -106,7 +106,7 @@ public class GethConfigBean {
     private final String GETH_MAX_BLOCKTIME = "geth.max.blocktime";
     private final String GETH_VOTE_CONTARCT_ADDRESS = "geth.vote.contract.addr";
     private final String GETH_CONSTELLATION_ENABLED = "geth.constellation.enabled";
-
+    
     public GethConfigBean() {
     }
 
@@ -119,7 +119,7 @@ public class GethConfigBean {
 //        AppConfig.initVendorConfig(new File(configFile));
         initBean();
     }
-
+    
     @PostConstruct
     private void initBean() {
         try {
@@ -128,7 +128,7 @@ public class GethConfigBean {
             LOG.error(ex.getMessage());
         }
     }
-
+    
     private void initGethBean() throws IOException, InterruptedException {
 
         // setup needed paths
@@ -156,7 +156,7 @@ public class GethConfigBean {
             LOG.error("Running on unsupported OS! Only Windows, Linux and Mac OS X are currently supported");
             throw new IllegalArgumentException("Running on unsupported OS! Only Windows, Linux and Mac OS X are currently supported");
         }
-
+        
         if (!ensureFileIsExecutable(gethPath)) {
             throw new IOException("Path does not exist or is not executable: " + gethPath);
         }
@@ -165,13 +165,13 @@ public class GethConfigBean {
 
         // init genesis block file (using vendor copy if necessary)
         String vendorGenesisDir = expandPath(baseResourcePath, "genesis");
-
+        
         genesisBlockFilename = expandPath(CONFIG_ROOT, "genesis_block.json");
         if (!new File(genesisBlockFilename).exists()) {
             String vendorGenesisBlockFile = FileUtils.join(vendorGenesisDir, "genesis_block.json");
             copyFile(new File(vendorGenesisBlockFile), new File(genesisBlockFilename));
         }
-
+        
         if (SystemUtils.IS_OS_WINDOWS) {
             genesisBlockFilename = genesisBlockFilename.replaceAll(File.separator + File.separator, "/");
             if (genesisBlockFilename.startsWith("/")) {
@@ -213,7 +213,7 @@ public class GethConfigBean {
             // null, init it
             setDataDirPath(expandPath(CONFIG_ROOT, "ethereum"));
         }
-
+        
         String identity = getIdentity();
         if (StringUtils.isBlank(identity)) {
             identity = System.getenv("USER");
@@ -222,15 +222,16 @@ public class GethConfigBean {
             }
         }
         setIdentity(identity);
-
+        
         if (LOG.isDebugEnabled()) {
             LOG.debug(StringUtils.toString(this));
         }
-
+        
         if (StringUtils.isBlank(EMBEDDED_NODE)) {
             //default to quorum
             setGethPath(quorumConfig.getQuorumPath());
-            String destination = getDataDirPath().concat("/constellation/");
+            String destination = System.getProperty("spring.config.location").replaceAll("file:", "")
+                    .replaceAll("application.properties", "/").concat("constellation-node/");
             quorumConfig.createKeys("node", destination);
             quorumConfig.createQuorumConfig("node", destination);
             setConstPidFileName(expandPath(CONFIG_ROOT, "constellation.pid"));
@@ -254,19 +255,19 @@ public class GethConfigBean {
         ensureFileIsExecutable(nodePath + File.separator + "node");
         ensureFileIsExecutable(nodePath + File.separator + "node.exe");
     }
-
+    
     public String getGethPath() {
         return gethPath;
     }
-
+    
     public void setGethPath(String gethPath) {
         this.gethPath = gethPath;
     }
-
+    
     public String getGethPidFilename() {
         return gethPidFilename;
     }
-
+    
     public void setGethPidFilename(String gethPidFilename) {
         this.gethPidFilename = gethPidFilename;
     }
@@ -284,39 +285,39 @@ public class GethConfigBean {
     public void setConstPidFileName(String constPidFileName) {
         this.constPidFileName = constPidFileName;
     }
-
+    
     public String getDataDirPath() {
         return props.getProperty(GETH_DATA_DIR);
     }
-
+    
     public void setDataDirPath(String dataDirPath) {
         props.setProperty(GETH_DATA_DIR, dataDirPath);
     }
-
+    
     public String getLogDir() {
         return props.getProperty(GETH_LOG_DIR);
     }
-
+    
     public void setLogDir(String logDir) {
         props.setProperty(GETH_LOG_DIR, logDir);
     }
-
+    
     public String getGenesisBlockFilename() {
         return genesisBlockFilename;
     }
-
+    
     public void setGenesisBlockFilename(String genesisBlockFilename) {
         this.genesisBlockFilename = genesisBlockFilename;
     }
-
+    
     public String getRpcUrl() {
         return props.getProperty(GETH_RPC_URL);
     }
-
+    
     public void setRpcUrl(String rpcUrl) {
         props.setProperty(GETH_RPC_URL, rpcUrl);
     }
-
+    
     public String getRpcPort() {
         String url = getRpcUrl();
         if (StringUtils.isBlank(url)) {
@@ -325,7 +326,7 @@ public class GethConfigBean {
         URI uri = URI.create(url);
         return Integer.toString(uri.getPort());
     }
-
+    
     public String getRpcApiList() {
         if (StringUtils.isBlank(EMBEDDED_NODE)) {
             if (props.getProperty(GETH_RPCAPI_LIST).contains("quorum")) {
@@ -337,7 +338,7 @@ public class GethConfigBean {
             return props.getProperty(GETH_RPCAPI_LIST);
         }
     }
-
+    
     public void setRpcApiList(String rpcApiList) {
         if (StringUtils.isBlank(EMBEDDED_NODE)) {
             if (rpcApiList.contains("quorum")) {
@@ -349,197 +350,197 @@ public class GethConfigBean {
             props.setProperty(GETH_RPCAPI_LIST, rpcApiList);
         }
     }
-
+    
     public String getGethNodePort() {
         return props.getProperty(GETH_NODE_PORT, DEFAULT_NODE_PORT);
     }
-
+    
     public void setGethNodePort(String gethNodePort) {
         props.setProperty(GETH_NODE_PORT, gethNodePort);
     }
-
+    
     public Boolean isAutoStart() {
         return Boolean.valueOf(get(GETH_AUTO_START, "false"));
     }
-
+    
     public void setAutoStart(Boolean autoStart) {
         props.setProperty(GETH_AUTO_START, autoStart.toString());
     }
-
+    
     public Boolean isAutoStop() {
         return Boolean.valueOf(get(GETH_AUTO_STOP, "false"));
     }
-
+    
     public void setAutoStop(Boolean autoStop) {
         props.setProperty(GETH_AUTO_STOP, autoStop.toString());
     }
-
+    
     public Integer getNetworkId() {
         return Integer.valueOf(get(GETH_NETWORK_ID, "1006"));
     }
-
+    
     public void setNetworkId(Integer networkId) {
         props.setProperty(GETH_NETWORK_ID, networkId.toString());
     }
-
+    
     public Integer getVerbosity() {
         return Integer.valueOf(get(GETH_VERBOSITY, "3"));
     }
-
+    
     public void setVerbosity(Integer verbosity) {
         props.setProperty(GETH_VERBOSITY, verbosity.toString());
     }
-
+    
     public Boolean isMining() {
         return Boolean.valueOf(get(GETH_MINING, "false"));
     }
-
+    
     public void setMining(Boolean mining) {
         props.setProperty(GETH_MINING, mining.toString());
     }
-
+    
     public String getIdentity() {
         return props.getProperty(GETH_IDENTITY);
     }
-
+    
     public void setIdentity(String identity) {
         props.setProperty(GETH_IDENTITY, identity);
     }
-
+    
     public String getBlockMaker() {
         return props.getProperty(GETH_BLOCK_MAKER);
     }
-
+    
     public void setBlockMaker(String blockMaker) {
         props.setProperty(GETH_BLOCK_MAKER, blockMaker);
     }
-
+    
     public String getVoteAccount() {
         return props.getProperty(GETH_VOTE_ACCOUNT);
     }
-
+    
     public void setVoteAccount(String voteAccount) {
         props.setProperty(GETH_VOTE_ACCOUNT, voteAccount);
     }
-
+    
     public Integer getMinBlockTime() {
         return StringUtils.isNotBlank(props.getProperty(GETH_MIN_BLOCKTIME)) ? Integer.valueOf(props.getProperty(GETH_MIN_BLOCKTIME)) : 2;
     }
-
+    
     public Integer getMaxBlockTime() {
         return StringUtils.isNotBlank(props.getProperty(GETH_MAX_BLOCKTIME)) ? Integer.valueOf(props.getProperty(GETH_MAX_BLOCKTIME)) : 5;
     }
-
+    
     public void setMinBlockTime(Integer minblockTime) {
         props.setProperty(GETH_MIN_BLOCKTIME, String.valueOf(minblockTime));
     }
-
+    
     public void setMaxBlockTime(Integer maxblockTime) {
         props.setProperty(GETH_MAX_BLOCKTIME, String.valueOf(maxblockTime));
     }
-
+    
     public String getVoteContractAddress() {
         return props.getProperty(GETH_VOTE_CONTARCT_ADDRESS);
     }
-
+    
     public void setVoteContractAddress(String address) {
         props.setProperty(GETH_VOTE_CONTARCT_ADDRESS, address);
     }
-
+    
     public Boolean isConstellationEnabled() {
         return StringUtils.isNotBlank(props.getProperty(GETH_CONSTELLATION_ENABLED))
                 ? Boolean.valueOf(props.getProperty(GETH_CONSTELLATION_ENABLED))
                 : Boolean.TRUE;
     }
-
+    
     public void setConstellationEnabled(Boolean isEnabled) {
         props.setProperty(GETH_CONSTELLATION_ENABLED, String.valueOf(isEnabled));
     }
-
+    
     public String getBinPath() {
         return binPath;
     }
-
+    
     public void setBinPath(String binPath) {
         this.binPath = binPath;
     }
-
+    
     public String getGethPasswordFile() {
         return gethPasswordFile;
     }
-
+    
     public void setGethPasswordFile(String gethPasswordFile) {
         this.gethPasswordFile = gethPasswordFile;
     }
-
+    
     public String getKeystorePath() {
         return keystorePath;
     }
-
+    
     public void setKeystorePath(String keystorePath) {
         this.keystorePath = keystorePath;
     }
-
+    
     public String getSolcPath() {
         return solcPath;
     }
-
+    
     public void setSolcPath(String solcPath) {
         this.solcPath = solcPath;
     }
-
+    
     public String getExtraParams() {
         return props.getProperty(GETH_EXTRA_PARAMS);
     }
-
+    
     public void setExtraParams(String extraParams) {
         props.setProperty(GETH_EXTRA_PARAMS, extraParams);
     }
-
+    
     public String getGenesisBlock() throws IOException {
         return FileUtils.readFileToString(new File(genesisBlockFilename));
     }
-
+    
     public void setGenesisBlock(String genesisBlock) throws IOException {
         FileUtils.writeStringToFile(new File(genesisBlockFilename), genesisBlock);
     }
-
+    
     public String getNodePath() {
         return nodePath;
     }
-
+    
     public void setNodePath(String nodePath) {
         this.nodePath = nodePath;
     }
-
+    
     public boolean isDbEnabled() {
         return StringUtils.isNotBlank(env.getProperty(GETH_DB_ENABLED)) || StringUtils.isNotBlank(System.getProperty(GETH_DB_ENABLED));
     }
-
+    
     public void setDbEnabled(String vendor) {
         props.setProperty(GETH_DB_ENABLED, vendor);
     }
-
+    
     public int getGethStartTimeout() {
         return Integer.parseInt(get(GETH_START_TIMEOUT, "10000"));
     }
-
+    
     public void setGethStartTimeout(int timeout) {
         props.setProperty(GETH_START_TIMEOUT, Integer.toString(timeout));
     }
-
+    
     public int getGethUnlockTimeout() {
         return Integer.parseInt(get(GETH_UNLOCK_TIMEOUT, "2000"));
     }
-
+    
     public void setGethUnlockTimeout(int timeout) {
         props.setProperty(GETH_UNLOCK_TIMEOUT, Integer.toString(timeout));
     }
-
+    
     public Boolean isQuorum() {
         return isQuorum;
     }
-
+    
     public void setQuorum(Boolean isQuorum) {
         this.isQuorum = isQuorum;
     }
@@ -602,5 +603,5 @@ public class GethConfigBean {
     public void setPublicKey(String publicKey) {
         this.publicKey = publicKey;
     }
-
+    
 }
