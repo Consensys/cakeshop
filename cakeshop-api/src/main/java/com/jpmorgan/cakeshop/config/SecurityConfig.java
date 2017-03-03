bf601c1866b6aabd1cc33d4409e636978c6c8268
Michael Kazansky <michael.kazansky@jpmchase.com>
2017-03-03 17:26:07 -0500

    removed setting only for 1 session

--- cakeshop-api/src/main/java/com/jpmorgan/cakeshop/config/SecurityConfig.java
+++ cakeshop-api/src/main/java/com/jpmorgan/cakeshop/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.jpmorgan.cakeshop.config;
 
 import com.jpmorgan.cakeshop.service.auth.impl.AuthenticationService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +9,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Autowired
     @Qualifier("authService")
@@ -26,6 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .authorizeRequests()
                 .antMatchers("/resources/**", "/user").permitAll()
@@ -34,7 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .and().httpBasic()
                 .and().logout().logoutSuccessUrl("/login").permitAll();
-        http.sessionManagement().maximumSessions(1);
 
         http.csrf().disable();
     }
