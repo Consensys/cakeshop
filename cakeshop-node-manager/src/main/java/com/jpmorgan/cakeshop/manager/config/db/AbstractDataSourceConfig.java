00798d3d64a94bf65a8fba27f4461e855f06c7ab
Michael Kazansky <michael.kazansky@jpmchase.com>
2017-04-06 13:04:53 -0400

    code clean up

--- cakeshop-node-manager/src/main/java/com/jpmorgan/cakeshop/manager/config/db/AbstractDataSourceConfig.java
+++ cakeshop-node-manager/src/main/java/com/jpmorgan/cakeshop/manager/config/db/AbstractDataSourceConfig.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
