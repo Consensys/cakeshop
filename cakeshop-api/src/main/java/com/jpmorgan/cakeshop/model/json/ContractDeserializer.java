b73e284525a9fd451c82b45a199af9516fdb3922
fixanoid <felix.shnir@gmail.com>
2017-07-14 15:55:43 -0400

    Resolving issues: #38, #39

--- cakeshop-api/src/main/java/com/jpmorgan/cakeshop/model/json/ContractDeserializer.java
+++ cakeshop-api/src/main/java/com/jpmorgan/cakeshop/model/json/ContractDeserializer.java
@@ -13,6 +13,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Lists;
 import com.jpmorgan.cakeshop.util.StringUtils;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -26,6 +27,7 @@ public class ContractDeserializer extends JsonDeserializer<ContractPostJsonReque
         if (null != node.get("privateFor")) {
             JsonNode privateForNode = node.get("privateFor");
             List<String> privateFor = null;
+            
             if (privateForNode.isArray()) {
                 privateFor = Lists.newArrayList();
                 for (Iterator<JsonNode> iter = privateForNode.elements(); iter.hasNext();) {
@@ -36,6 +38,7 @@ public class ContractDeserializer extends JsonDeserializer<ContractPostJsonReque
                     privateFor = Lists.newArrayList(node.get("privateFor").textValue());
                 }
             }
+            
             request.setPrivateFor(privateFor);
         }
 
@@ -46,9 +49,11 @@ public class ContractDeserializer extends JsonDeserializer<ContractPostJsonReque
         if (null != node.get("code_type")) {
             request.setCode_type(node.get("code_type").textValue());
         }
+        
         if (null != node.get("from")) {
             request.setFrom(node.get("from").textValue());
         }
+        
         if (null != node.get("binary")) {
             request.setBinary(node.get("binary").textValue());
         }
@@ -56,32 +61,52 @@ public class ContractDeserializer extends JsonDeserializer<ContractPostJsonReque
         if (null != node.get("privateFrom")) {
             request.setPrivateFrom(node.get("privateFrom").textValue());
         }
+        
         if (null != node.get("address")) {
             request.setAddress(node.get("address").textValue());
         }
+        
         if (null != node.get("method")) {
             request.setMethod(node.get("method").textValue());
         }
+        
         if (null != node.get("blockNumber")) {
             request.setBlockNumber(node.get("blockNumber").textValue());
         }
+        
         if (null != node.get("optimize")) {
             request.setOptimize(node.get("optimize").booleanValue());
         }
 
         if (null != node.get("args")) {
             JsonNode argsNode = node.get("args");
-            List<String> args = null;
+            List<Object> args = null;
+
             if (argsNode.isArray()) {
                 args = Lists.newArrayList();
+
                 for (Iterator<JsonNode> iter = argsNode.elements(); iter.hasNext();) {
-                    args.add(iter.next().asText());
+                	JsonNode arrEntry = iter.next();
+
+                	if (arrEntry.isArray()) {
+                		List <Object> argArr = new ArrayList<>();
+
+                		for (Iterator <JsonNode> iter2 = arrEntry.elements(); iter2.hasNext();) {
+                			JsonNode arrEntry2 = iter2.next();
+                			argArr.add(arrEntry2.asText());
+                		}
+
+                		args.add(argArr);
+                	} else {
+                		args.add(arrEntry.asText());
+                	}
                 }
             } else {
                 if (StringUtils.isNotBlank(node.get("args").textValue())) {
                     args = Lists.newArrayList(node.get("args").textValue());
                 }
             }
+
             request.setArgs(args.toArray());
         }
 
