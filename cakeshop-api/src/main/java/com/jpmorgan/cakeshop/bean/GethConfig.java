6fc190435b0984b1d5d8373965c63dcc27b3b145
bmcd87 <bradmcdermott@gmail.com>
2019-10-30 15:38:05 -0400

    Changes default ports to not conflict with 7nodes

--- cakeshop-api/src/main/java/com/jpmorgan/cakeshop/bean/GethConfig.java
+++ cakeshop-api/src/main/java/com/jpmorgan/cakeshop/bean/GethConfig.java
@@ -11,6 +11,14 @@ import com.quorum.tessera.config.SslAuthenticationMode;
 import com.quorum.tessera.config.SslTrustMode;
 import com.quorum.tessera.config.builder.ConfigBuilder;
 import com.quorum.tessera.config.builder.KeyDataBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,18 +26,7 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Properties;
-import javax.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
+import java.util.*;
 
 @Component
 public class GethConfig {
@@ -420,7 +417,7 @@ public class GethConfig {
     }
 
     public String getGethTransactionManagerUrl() {
-        return get(GETH_TRANSACTION_MANAGER_URL, "http://127.0.0.1:9000/");
+        return get(GETH_TRANSACTION_MANAGER_URL, "http://127.0.0.1:9102/");
     }
 
     public void setGethTransactionManagerUrl(String url) {
@@ -429,7 +426,7 @@ public class GethConfig {
 
     public List<String> getGethTransactionManagerPeers() {
         return Lists
-            .newArrayList(get(GETH_TRANSACTION_MANAGER_PEERS, "http://localhost:9000/").split(","));
+            .newArrayList(get(GETH_TRANSACTION_MANAGER_PEERS, "http://localhost:9102/").split(","));
     }
 
     public void setGethTransactionManagerPeers(List<String> peers) {
