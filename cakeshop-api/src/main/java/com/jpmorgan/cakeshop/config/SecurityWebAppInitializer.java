ac4cb650d951ea32d6b902ecdea2e7b484de554b
Brad McDermott <bradmcdermott@gmail.com>
2020-08-06 16:50:58 -0400

    Removes unused template stuff and deps, fixes test, updates dep versions

--- cakeshop-api/src/main/java/com/jpmorgan/cakeshop/config/SecurityWebAppInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.jpmorgan.cakeshop.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebAppInitializer {
-    //extends AbstractSecurityWebApplicationInitializer {
-
-}
